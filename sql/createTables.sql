66fa3344b662c5cb79d2b63273a2e4a3da4cf98f
Rodrigo Wahl <lwahlnayar@gmail.com>
2019-02-15 21:42:10 +0100

    voucher table created, fixed retailer table. basics done

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,10 +1,10 @@
+DROP TABLE IF EXISTS vouchers;
 DROP TABLE IF EXISTS retailers;
 
 CREATE TABLE retailers(
   id SERIAL PRIMARY KEY,
-  name VARCHAR(200) NOT NULL,
+  name VARCHAR(200) NOT NULL UNIQUE,
   quality VARCHAR(5) NOT NULL,
-  active_vouchers INT NOT NULL,
   published BOOLEAN NOT NULL,
   indexed BOOLEAN NOT NULL,
   affiliate_network VARCHAR(300),
@@ -12,46 +12,73 @@ CREATE TABLE retailers(
   KW2 VARCHAR(100),
   KW3 VARCHAR(100),
   KW4 VARCHAR(100),
-  url VARCHAR(100) NOT NULL
+  url VARCHAR(100) NOT NULL,
+  static_content_top VARCHAR(350),
+  static_content_left VARCHAR(500),
+  static_content_right VARCHAR(500),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE vouchers(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR (150) NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  type VARCHAR(4) NOT NULL,
+  retailer_id INT REFERENCES retailers(id) NOT NULL,
+  retailer_name VARCHAR(200) REFERENCES retailers(name) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
-INSERT INTO retailers (name, quality, active_vouchers, published, indexed, KW1, url) VALUES (
+INSERT INTO retailers (name, quality, published, indexed, KW1, url) VALUES (
     'Zalando',
     'AAA',
-    0,
     'no',
     'no',
     'zalando promo code',
     'zalando-promo-code'
 );
 
-INSERT INTO retailers (name, quality, active_vouchers, published, indexed, KW1, url) VALUES (
+INSERT INTO retailers (name, quality, published, indexed, KW1, url) VALUES (
     'Amazon',
     'AAA',
-    0,
     'no',
     'no',
     'amazon discount code',
     'amazon-promo-code'
 );
 
-INSERT INTO retailers (name, quality, active_vouchers, published, indexed, KW1, url) VALUES (
+INSERT INTO retailers (name, quality, published, indexed, KW1, url) VALUES (
     'GoDaddy',
     'AAA',
-    0,
     'no',
     'no',
     'godaddy discount code',
     'godaddy-discount-code'
 );
 
-INSERT INTO retailers (name, quality, active_vouchers, published, indexed, KW1, url) VALUES (
+INSERT INTO retailers (name, quality, published, indexed, KW1, url) VALUES (
     'Vueling',
     'AAA',
-    0,
     'no',
     'no',
     'vueling promo code',
     'vueling-promo-code'
+);
+
+-- vouchers
+INSERT INTO vouchers (title, description, type, retailer_name, retailer_id) VALUES (
+  '50% discount code on all shoes',
+  'Get your Zalando discount code for all shoes in the Zalando store!',
+  'Code',
+  'Zalando',
+  1
+);
+
+INSERT INTO vouchers (title, description, type, retailer_name, retailer_id) VALUES (
+  '25% off discount code on yellow socks!',
+  'Looking for yellow socks! We offer you a code of 25% Zalando discount for all yellow socks!',
+  'Code',
+  'Zalando',
+  1
 );
