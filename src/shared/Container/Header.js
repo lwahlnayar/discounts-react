29d1e6f917aaea939f76c470c286da81393f8448
Rodrigo Wahl <lwahlnayar@gmail.com>
2019-02-19 04:40:01 +0100

    fixed header position, added wrapper around header, added window

--- src/shared/Container/Header.js
+++ src/shared/Container/Header.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import window from "global";
 
 import "./Header.css";
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            prevScrollUp: window.pageYOffset,
+            visible: true
+        };
+    }
+
     render() {
         return (
-            <header>
-                <div className="header-elements center">
-                    <div>Logo</div>
-                    <div
-                        style={{
-                            "border-color": "black"
-                        }}
-                    >
-                        Search Component Here
+            <div className="header-grid">
+                <header className={!this.state.visible && "header-hidden"}>
+                    <div className="header-elements center">
+                        <div>Logo</div>
+                        <div
+                            style={{
+                                border: "1px solid black"
+                            }}
+                        >
+                            Search Component Here
+                        </div>
+                        <ul>
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                        </ul>
                     </div>
-                    <ul>
-                        <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </header>
+                </header>
+            </div>
         );
     }
 }
