7fb02aae6e052b644d512df2b1d22698aa1479ea
Rodrigo Wahl <lwahlnayar@gmail.com>
2019-02-21 00:09:12 +0100

    head mobile viewport

--- src/server/renderHtml.js
+++ src/server/renderHtml.js
@@ -2,6 +2,7 @@ export default function renderHtml(html) {
     return `<!doctype html>
   <head>
   <link rel="stylesheet" type="text/css" href="main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <title></title>
     <body>
